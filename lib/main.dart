b5beb01d8a4a6b3116e8edb8816c287b0cf3804c
Malventi, Giulio <Giulio.Malventi@nov.com>
2021-12-08 18:44:07 +0100

    New counter with fab

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:flutter_palette/flutter_palette.dart';
@@ -98,12 +99,13 @@ class _MainPageState extends State<MainPage> {
   var colors = [];
   var counters = SplayTreeMap();
   double maxY = 0;
-  var period = GetStorage().read('period');
+  var period = GetStorage().read('period') ?? 'days';
   Map<String, List<FlSpot>> spots = {};
   var _today = 0;
 
   late String exportPath;
   late DocumentReference<Map<String, dynamic>> userRef;
+  bool fabVisible = true;
 
   @override
   void initState() {
@@ -130,6 +132,11 @@ class _MainPageState extends State<MainPage> {
     getExternalStorageDirectory().then((externalStorage) {
       exportPath = '${externalStorage?.path}/data.json';
     });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
   }
 
   void importData() async {
@@ -171,6 +178,23 @@ class _MainPageState extends State<MainPage> {
     }
     userRef.update(data);
     ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Data imported')));
+  }
+
+  void newCounter() async {
+    var alreadyUsed = false;
+    String? name;
+    while (true) {
+      name = (await showTextInputDialog(
+          context: context,
+          textFields: const [DialogTextField()],
+          message: alreadyUsed ? '$name is already used' : 'name the counter'))?[0];
+      if ([null, ''].contains(name)) return;
+      if (!counters.containsKey(name)) break;
+      alreadyUsed = true;
+    }
+    userRef.update({
+      'counters.$name': {today().toString(): 0}
+    });
   }
 
   void prepareChart() => setState(() {
@@ -255,6 +279,9 @@ class _MainPageState extends State<MainPage> {
                 ),
               ],
             ))),
+        floatingActionButton: Visibility(
+            visible: fabVisible,
+            child: FloatingActionButton(onPressed: newCounter, child: const Icon(Icons.add))),
         body: Flex(direction: portrait ? Axis.vertical : Axis.horizontal, children: [
           Expanded(
               flex: portrait ? 45 : 60,
@@ -287,80 +314,70 @@ class _MainPageState extends State<MainPage> {
               flex: portrait ? 55 : 40,
               child: Padding(
                   padding: const EdgeInsets.only(top: 10),
-                  child: ListView.separated(
-                    separatorBuilder: (_, __) =>
-                        Divider(height: 5, color: Theme.of(context).canvasColor),
-                    itemCount: counters.keys.isNotEmpty ? counters.keys.length + 1 : 1,
-                    itemBuilder: (context, index) {
-                      if (index == counters.keys.length) {
-                        return Align(
-                            alignment: Alignment.center,
-                            child: Padding(
-                                padding: const EdgeInsets.only(bottom: 5),
-                                child: ElevatedButton(
-                                    child: const Text('New counter'),
-                                    onPressed: () async {
-                                      var alreadyUsed = false;
-                                      String? name;
-                                      while (true) {
-                                        name = (await showTextInputDialog(
-                                            context: context,
-                                            textFields: const [DialogTextField()],
-                                            message: alreadyUsed
-                                                ? '$name is already used'
-                                                : 'name the counter'))?[0];
-                                        if ([null, ''].contains(name)) return;
-                                        if (!counters.containsKey(name)) break;
-                                        alreadyUsed = true;
-                                      }
-                                      userRef.update({
-                                        'counters.$name': {today().toString(): 0}
-                                      });
-                                    })));
-                      }
-                      final color = colors[index];
-                      final name = List.of(counters.keys)[index];
-                      final _today = today().toString();
-                      return ListTile(
-                        tileColor: Theme.of(context).cardColor,
-                        title: Text(
-                          name,
-                          style: TextStyle(color: color),
-                        ),
-                        trailing: Row(mainAxisSize: MainAxisSize.min, children: [
-                          IconButton(
-                              color: color,
-                              onPressed: () {
-                                final _today = today().toString();
-                                userRef.update(
-                                    {'counters.$name.$_today': (counters[name][_today] ?? 0) + 1});
-                              },
-                              icon: const Icon(Icons.add)),
-                          IconButton(
-                              color: color,
-                              onPressed: () {
-                                if (counters[name].containsKey(_today) &&
-                                    counters[name][_today] > 0) {
-                                  userRef.update(
-                                      {'counters.$name.$_today': counters[name][_today] - 1});
-                                }
-                              },
-                              icon: const Icon(Icons.remove)),
-                          IconButton(
-                              color: color,
-                              onPressed: () async {
-                                if (await showModalActionSheet(context: context, actions: [
-                                      SheetAction(key: 'remove', label: 'Remove $name')
-                                    ]) ==
-                                    'remove') {
-                                  userRef.update({'counters.$name': FieldValue.delete()});
-                                }
-                              },
-                              icon: const Icon(Icons.close)),
-                        ]),
-                      );
-                    },
-                  ))),
+                  child: NotificationListener<ScrollNotification>(
+                      onNotification: (notification) {
+                        if (notification.depth == 0) {
+                          if (fabVisible) {
+                            if (notification is OverscrollNotification &&
+                                notification.overscroll > 0) {
+                              setState(() => fabVisible = false);
+                            }
+                          } else if (notification is UserScrollNotification &&
+                              notification.direction == ScrollDirection.forward) {
+                            setState(() => fabVisible = true);
+                          }
+                        }
+                        return false;
+                      },
+                      child: ListView.separated(
+                        separatorBuilder: (_, __) =>
+                            Divider(height: 5, color: Theme.of(context).canvasColor),
+                        itemCount: counters.keys.length,
+                        itemBuilder: (context, index) {
+                          final color = colors[index];
+                          final name = List.of(counters.keys)[index];
+                          final _today = today().toString();
+                          return ListTile(
+                            tileColor: Theme.of(context).cardColor,
+                            title: Text(
+                              name,
+                              style: TextStyle(color: color),
+                            ),
+                            trailing: Row(mainAxisSize: MainAxisSize.min, children: [
+                              IconButton(
+                                  color: color,
+                                  onPressed: () {
+                                    final _today = today().toString();
+                                    userRef.update({
+                                      'counters.$name.$_today': (counters[name][_today] ?? 0) + 1
+                                    });
+                                  },
+                                  icon: const Icon(Icons.add)),
+                              IconButton(
+                                  color: color,
+                                  onPressed: () {
+                                    if (counters[name].containsKey(_today) &&
+                                        counters[name][_today] > 0) {
+                                      userRef.update(
+                                          {'counters.$name.$_today': counters[name][_today] - 1});
+                                    }
+                                  },
+                                  icon: const Icon(Icons.remove)),
+                              IconButton(
+                                  color: color,
+                                  onPressed: () async {
+                                    if (await showModalActionSheet(context: context, actions: [
+                                          SheetAction(key: 'remove', label: 'Remove $name')
+                                        ]) ==
+                                        'remove') {
+                                      userRef.update({'counters.$name': FieldValue.delete()});
+                                    }
+                                  },
+                                  icon: const Icon(Icons.close)),
+                            ]),
+                          );
+                        },
+                      )))),
         ]));
   }
 }
